1d6a3a8e0505556b37b51efdc0a11518d8e54dc4
Murillo Morgado <mumorgadok@hotmail.com>
2024-01-08 22:11:44 -0300

    Feito a implementacao do treinador

--- src/main/java/pokemon/demo/service/Impl/TreinadorServiceImpl.java
+++ src/main/java/pokemon/demo/service/Impl/TreinadorServiceImpl.java
@@ -2,39 +2,64 @@ package pokemon.demo.service.Impl;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
 import pokemon.demo.model.Treinador;
+import pokemon.demo.model.exceotion.InternalServerException;
+import pokemon.demo.model.exceotion.NotFoundException;
+import pokemon.demo.repository.TreinadorRespository;
 import pokemon.demo.service.TreinadorService;
+
+@Service
+@RequiredArgsConstructor
 
 public class TreinadorServiceImpl implements TreinadorService {
 
+  private final TreinadorRespository treinadorRespository;
+
   @Override
   public List<Treinador> listarTreinador() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'listarTreinador'");
+    return treinadorRespository.findAll();
   }
 
   @Override
   public Treinador buscarTreinador(Long idTreinador) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'buscarTreinador'");
+    return treinadorRespository.findById(idTreinador).orElseThrow(() -> new NotFoundException("Falha ao procurar o Treinador"));
   }
 
   @Override
   public Treinador criarTreinador(Treinador treinador) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'criarTreinador'");
+    try {
+      return treinadorRespository.save(treinador);
+    } catch (Exception e) {
+      throw new InternalServerException("Falha ao criar Treinador");
+    }
   }
 
   @Override
   public void atualizarTreinador(Long idTreinador, Treinador treinador) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'atualizarTreinador'");
+    try {
+      Treinador treinadorDB = buscarTreinador(idTreinador);
+
+      BeanUtils.copyProperties(treinador, treinadorDB);
+
+      treinadorRespository.save(treinadorDB);
+    } catch (Exception e) {
+      throw new InternalServerException("Falha ao atualizar o Treinador");
+    }
   }
 
   @Override
   public void deletarTreinador(Long idTreinador) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'deletarTreinador'");
+try {
+  Treinador treinadorDB = buscarTreinador(idTreinador);
+
+  treinadorRespository.delete(treinadorDB);
+} catch (Exception e) {
+  throw new InternalServerException("Falha ao deletar o Treinador");
+}
   }
   
 }
